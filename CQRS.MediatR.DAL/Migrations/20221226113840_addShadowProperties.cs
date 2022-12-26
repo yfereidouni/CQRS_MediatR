1fdf97213bd7e7a03fa3533a54c40e308e5456cf
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-26 12:42:54 +0100

    Interceptors was configured.

---
+++ CQRS.MediatR.DAL/Migrations/20221226113840_addShadowProperties.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CQRS.MediatR.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addShadowProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreateBy",
+                table: "Tags",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Tags",
+                type: "datetime2",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdateBy",
+                table: "Tags",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Tags",
+                type: "datetime2",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateBy",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateBy",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Tags");
+        }
+    }
+}
